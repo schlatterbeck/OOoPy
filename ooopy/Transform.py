16358d7de3753126a6e99afc15be8d39a14cc4f5
Ralf Schlatterbeck <rsc@runtux.com>
2005-03-04 15:23:20 +0000

    LGPL for the lib files
    added test file and modified software to use it.

--- ooopy/Transform.py
+++ ooopy/Transform.py
@@ -1,4 +1,26 @@
 #!/usr/bin/env python
+# -*- coding: iso-8859-1 -*-
+# Copyright (C) 2005 Dr. Ralf Schlatterbeck Open Source Consulting.
+# Reichergasse 131, A-3411 Weidling.
+# Web: http://www.runtux.com Email: office@runtux.com
+# All rights reserved
+# ****************************************************************************
+#
+# This library is free software; you can redistribute it and/or modify
+# it under the terms of the GNU Library General Public License as
+# published by the Free Software Foundation; either version 2 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Library General Public License for more details.
+#
+# You should have received a copy of the GNU Library General Public
+# License along with this program; if not, write to the Free Software
+# Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+# ****************************************************************************
+
 import OOoPy
 import time
 import re
@@ -311,15 +333,19 @@ class Addpagebreak_Transform (Transform) :
 if __name__ == '__main__' :
     from StringIO import StringIO
     sio = StringIO ()
-    o   = OOoPy.OOoPy (infile = 'zfr.sxw', outfile = sio)
+    o   = OOoPy.OOoPy (infile = 'test.sxw', outfile = sio)
     t   = Transformer \
         ( Autoupdate_Transform ()
         , Editinfo_Transform   ()
         , Field_Replace_Transform 
             ( replace =
-                { 'abo.payer.salutation' : 'Frau'
-                , 'abo.payer.firstname'  : ''
-                , 'abo.payer.lasttname'  : 'Musterfrau'
+                { 'salutation' : ''
+                , 'firstname'  : 'Erika'
+                , 'lastname'   : 'Musterfrau'
+                , 'street'     : 'Beispielstrasse 42'
+                , 'country'    : 'D'
+                , 'postalcode' : '00815'
+                , 'city'       : 'Niemandsdorf'
                 }
             )
         , Addpagebreak_Style_Transform ()
@@ -328,6 +354,6 @@ if __name__ == '__main__' :
     t.transform (o)
     o.close ()
     ov  = sio.getvalue ()
-    f   = open ("out.sxw", "w")
+    f   = open ("testout.sxw", "w")
     f.write (ov)
     f.close ()
