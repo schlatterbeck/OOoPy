ffbfaa34a54a3921914dc7f7dbd91c7229432d40
Ralf Schlatterbeck <rsc@runtux.com>
2020-02-06 21:12:25 +0100

    Fix copyright, remove coding cookies

--- ooopy/Transforms.py
+++ ooopy/Transforms.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python
-# -*- coding: iso-8859-1 -*-
-# Copyright (C) 2005-14 Dr. Ralf Schlatterbeck Open Source Consulting.
+# Copyright (C) 2005-20 Dr. Ralf Schlatterbeck Open Source Consulting.
 # Reichergasse 131, A-3411 Weidling.
 # Web: http://www.runtux.com Email: office@runtux.com
 # All rights reserved
--- ooopy/autosuper.py
+++ ooopy/autosuper.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python
-# -*- coding: iso-8859-1 -*-
 # Copyright (C) 2020 Dr. Ralf Schlatterbeck Open Source Consulting.
 # Reichergasse 131, A-3411 Weidling.
 # Web: http://www.runtux.com Email: office@runtux.com
